f001e171728b677009ebaff0e8665391f1b78e65
ankitlohiya4 <ankitlohiya4@gmail.com>
2022-09-22 21:49:55 +0530

    wrong code

--- data_processing.py
+++ data_processing.py
@@ -2,3 +2,5 @@ print("This file will contain all the code related to data processing")
 print("Checking what happens when code is committed without any message")
 
 print("Test code")
+
+print("Wrong Code")
