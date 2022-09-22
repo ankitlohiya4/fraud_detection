f7c709688f88c7efb40db9e51b1a7ac20b5aa70c
ankitlohiya4 <ankitlohiya4@gmail.com>
2022-09-22 21:50:32 +0530

    Revert "wrong code"
    This reverts commit f001e171728b677009ebaff0e8665391f1b78e65.

--- data_processing.py
+++ data_processing.py
@@ -2,5 +2,3 @@ print("This file will contain all the code related to data processing")
 print("Checking what happens when code is committed without any message")
 
 print("Test code")
-
-print("Wrong Code")
